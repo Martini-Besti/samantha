a84f9cbe0189a91b74d1c821df31b1dd93912eef
Martina Best <martinabest@mac.home>
2025-03-07 10:18:14 +0000

    neon install

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     ".railway.app",  # for production on Railway
     "127.0.0.1",  # for local development
     "localhost",  # for local development
-    "samanthatheclair.com"
+    "samanthatheclair.com",
 ]
 if DEBUG:
     ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
@@ -91,6 +91,16 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+if DATABASE_URL is not None:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL, conn_max_age=CONN_MAX_AGE, conn_health_checks=True
+        )
+    }
 
 
 # Password validation
